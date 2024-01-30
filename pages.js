c02788303eea1d1c309569a8e30b357910bca9e7
naveenkumar80 <naveenkuumar80032@gmail.com>
2024-01-30 22:49:43 +0530

    update

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/hashirshoaeb/hashirshoaeb.github.io.git";
+const repoURL = "https://naveenkumar80.github.io/portfolio/";
 
 ghpages.publish(
   pathname,
