bfab24292871a47f32cd2f3518a88ea5a7f85e8e
naveenkumar80 <naveenkuumar80032@gmail.com>
2024-01-30 22:39:44 +0530

    update

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,9 +6,9 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "Naveen",
   middleName: "",
-  lastName: "Shoaib",
+  lastName: "Kumar",
   message: " Passionate about changing the world with technology. ",
   icons: [
     {
@@ -55,7 +55,7 @@ const about = {
   imageSize: 375,
   message:
     "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
-  resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+  resume: "https://drive.google.com/file/d/1g2yTpzmRVoimEbXf8xLSz231oujZx_b1/view?usp=drive_link",
 };
 
 // PROJECTS SECTION
